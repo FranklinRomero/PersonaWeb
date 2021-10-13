e871994c47d06bdd964200972ac27c9cfd961698
Franklin <a2020115522@estudiantes.upsa.edu.bo>
2021-10-12 20:41:15 -0400

    Agregar archivos de proyecto.

---
+++ PersonaWeb/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PersonaWeb.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+                
+        }
+        public DbSet<PersonaWeb.Models.Person> Person { get; set; }
+    }
+}
---
+++ PersonaWeb/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonaWeb.Models
+
+{
+    public enum SexType
+    {
+        fem = 0,
+        masc = 1,
+    }
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "Porfavor ingrese entre 5 a 60 caracteres")]
+        [Display(Name = "Nombre de la persona")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Sexo")]
+        public SexType Sex { get; set; }
+
+        [Display(Name = "Pasa tiempo")]
+        public string Hobbie { get; set; }
+    }
+}
