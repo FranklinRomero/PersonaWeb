f7ccbaf0b19e6bda4c77268ccc0ebb08facca59b
Franklin <a2020115522@estudiantes.upsa.edu.bo>
2021-11-03 18:43:23 -0400

    Seguridad 1

---
+++ PersonaAPI/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿
+namespace PersonaAPI.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using PersonaApi.Services;
+    using PersonaAPI.Models;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(AuthenticateRequest model)
+        {
+            var response = _userService.Authenticate(model);
+
+            if (response == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(response);
+        }
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+    }
+
+}
---
+++ PersonaAPI/Models/AuthenticateRequest.cs
@@ -0,0 +1,12 @@
+﻿
+namespace PersonaAPI.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class AuthenticateRequest
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
